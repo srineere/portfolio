08e30bd0ee6fe2b35ecacd45743dd2163fa0cd53
Srineer Kaleri <srineerkaleri@gmail.com>
2023-07-08 13:19:13 +0530

    made changes to experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -11,10 +11,18 @@ function Experience() {
       <h1>Experience</h1>
         {/* <div className="line"></div> */}
         <div className="experiences">
+          <h2 className="h2e">Tekion Corp</h2>
+          <p>I worked here Engineering Intern, where I Developed several RESTful APIs using Django framework and MongoDB as the backend database, ensuring efficient and scalable data storage and retrieval. </p>
+          <p>Used: Django, MongoDb, MySql, Pytest</p>
+          <h4> Jan'23 - July'23  </h4>
+        </div>
+        <br/>
+        <div className="line"></div>
+        <div className="experiences">
           <h2 className="h2e">RMgX Technologies</h2>
-          <p>I worked here Web-Development intern, where I worked on developing many components using Angular and Ngrx. </p>
+          <p>I worked here Web-Development Intern, where I worked on developing many components using Angular and Ngrx. </p>
           <p>Used: Angular Js, NodeJs</p>
-          <h4> Sep'21 - Dec'21  </h4>
+          <h4> Feb'22 - Apr'22  </h4>
         </div>
         <br/>
         <div className="line"></div>
@@ -30,14 +38,14 @@ function Experience() {
           <h2 className="h2e">GDSC Core Member</h2>
           <p>I am core member of Web Development team in GDSC IIITS. I have so far taken a session and worked on the GDSC IIITS website.</p>
           <p>Used: React</p>
-          <h4> Nov'21 - Present  </h4>
+          <h4> Sep'21 - Jun'22  </h4>
         </div>
         <br/>
         <div className="line"></div>
         <div className="experiences">
           <h2 className="h2e">Member Of IOTA and Gradient Clubs</h2>
           <p>I am a member of both IOTA and Gradient clubs which are development and competetive programming related clubs respectively.</p>
-          <h4> Mar'21 - Present  </h4>
+          <h4> Oct'20 - Dec'21  </h4>
         </div>
         <br/>
         <div className="line"></div>
